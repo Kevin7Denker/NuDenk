b0e40a8708d70a4dc440edd27b0fa54dbe71e9d5
Kevin Denker <oficialdenker@gmail.com>
2024-12-14 14:52:20 -0300

    Update Routes.java

--- bank/src/main/java/com/bank/routes/Routes.java
+++ bank/src/main/java/com/bank/routes/Routes.java
@@ -2,6 +2,7 @@ package com.bank.routes;
 
 import com.bank.controller.AuthController;
 import com.bank.controller.IndexController;
+import com.bank.middleware.AuthMiddleware;
 
 import io.javalin.Javalin;
 
@@ -9,16 +10,23 @@ public class Routes {
 
     private final IndexController indexController = new IndexController();
     private final AuthController authController = new AuthController();
+    private final AuthMiddleware middleware = new AuthMiddleware();
 
     public void RoutesInit(Javalin app) {
-        app.get("/", indexController.welcome);
-        app.get("/show", authController.show);
 
-        app.get("/register", authController.signUp);
-        app.post("/register", authController.register);
+        app.before(middleware.authMiddleware);
+        if (middleware.authMiddleware != null) {
+            app.get("/", authController.home);
+            app.get("/show", authController.show);
 
-        app.get("/login", authController.signIn);
-        app.post("/login", authController.login);
+        } else {
+            app.get("/", indexController.welcome);
+            app.get("/register", authController.signUp);
+            app.post("/register", authController.register);
+
+            app.get("/login", authController.signIn);
+            app.post("/login", authController.login);
+        }
 
     }
 
