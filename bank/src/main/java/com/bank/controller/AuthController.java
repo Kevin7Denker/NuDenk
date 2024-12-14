4936ef0a57d78bb381869c67d7b13e4dcc482dee
Kevin Denker <oficialdenker@gmail.com>
2024-12-14 14:51:58 -0300

    Add lógica de contexto e token

--- bank/src/main/java/com/bank/controller/AuthController.java
+++ bank/src/main/java/com/bank/controller/AuthController.java
@@ -4,11 +4,15 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.bank.hooks.AuthContext;
 import com.bank.models.ClientStandard;
 import com.bank.repository.Auth;
+import com.bank.services.Token;
 
 import io.javalin.http.Context;
+import io.javalin.http.Cookie;
 import io.javalin.http.Handler;
+import io.jsonwebtoken.Claims;
 
 public class AuthController {
 
@@ -16,7 +20,6 @@ public class AuthController {
     private ArrayList<ClientStandard> clientes = new ArrayList<>();
 
     public Handler show = (Context ctx) -> {
-
         clientes = authRepo.getClientes();
 
         if (clientes == null || clientes.isEmpty()) {
@@ -26,20 +29,17 @@ public class AuthController {
         }
 
         Map<String, Object> dados = new HashMap<>();
-
         dados.put("clientes", clientes);
 
         ctx.render("show.html", dados);
-
     };
+
     public Handler signUp = (Context ctx) -> {
         ctx.render("/auth/register.html");
     };
 
     public Handler register = (Context ctx) -> {
-
         try {
-
             String nome = ctx.formParam("nome");
             String cpf = ctx.formParam("cpf");
             String email = ctx.formParam("email");
@@ -52,12 +52,11 @@ public class AuthController {
 
             ctx.redirect("/show");
 
-        } catch (Exception e) {
+        } catch (NumberFormatException e) {
             ctx.status(400);
             ctx.result(e.getMessage());
             ctx.redirect("/register.html");
         }
-
     };
 
     public Handler signIn = (Context ctx) -> {
@@ -65,21 +64,71 @@ public class AuthController {
     };
 
     public Handler login = (Context ctx) -> {
-
         try {
             String email = ctx.formParam("email");
             String password = ctx.formParam("password");
 
-            authRepo.login(ctx, email, password);
+            ClientStandard client = authRepo.login(ctx, email, password);
 
-            ctx.redirect("/private/home.html");
+            if (client != null) {
 
-        } catch (Exception e) {
+                String token = Token.gerarToken(client.getEmail(), client.getNome(), client.getSaldo());
+                Cookie cookie = new Cookie("jwtToken", token);
+                cookie.setMaxAge(3600);
+                cookie.setHttpOnly(true);
+                cookie.setSecure(true);
+                ctx.cookie(cookie);
+
+                ctx.redirect("/show");
+            } else {
+                ctx.status(401).result("Credenciais inválidas.");
+                ctx.redirect("/show");
+            }
+        } catch (Error e) {
             ctx.status(400);
             ctx.result(e.getMessage());
-            ctx.redirect("/login.html");
-
+            System.out.println("erro: " + e.getMessage());
+            ctx.redirect("/login");
+        } catch (Exception e) {
+            ctx.status(500);
+            ctx.result("Erro inesperado: " + e.getMessage());
+            ctx.redirect("/login");
         }
-
     };
+
+    public Handler logout = (Context ctx) -> {
+        ctx.removeCookie("jwtToken");
+        ctx.redirect("/login");
+    };
+
+    public Handler checkAuth = (Context ctx) -> {
+        String token = ctx.cookie("jwtToken");
+
+        if (token != null) {
+            try {
+                Claims claims = Token.validarToken(token);
+                System.out.println("Usuário autenticado: " + claims.getSubject());
+                ctx.json("Usuário está logado: " + claims.getSubject());
+            } catch (Exception e) {
+                ctx.status(401).json("Token inválido.");
+            }
+        } else {
+            ctx.status(401).json("Usuário não está logado.");
+        }
+    };
+
+    public Handler home = (Context ctx) -> {
+
+        AuthContext authContext = ctx.attribute("authContext");
+
+        if (authContext != null && authContext.isLoggedIn()) {
+            String userName = authContext.getUserName();
+            double userBalance = authContext.getUserBalance();
+
+            ctx.json("Bem-vindo " + userName + "! Seu saldo é: " + userBalance);
+        } else {
+            ctx.status(403).json("Acesso negado.");
+        }
+    };
+
 }
